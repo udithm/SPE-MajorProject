ce4d245a1a6e2b6936daca6a08b609c16ecd88e7
jhanwweereddy <Shree.Jhanwwee@iiitb.org>
2022-04-30 23:30:42 +0530

    models added

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const expressValidator = require('express-validator');
+const passport = require('passport');
+const localStrategy = require('./auth/local');
+const jwtStrategy = require('./auth/jwt');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(expressValidator());
+app.use(morgan('common'));
+app.use(passport.initialize());
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+require('./routes')(app);
+
+module.exports = app;
---
+++ server/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    port: 8080,
+    db: {
+      prod: process.env.DATABASE_URL || 'mongodb://localhost/NiceFind',
+      test: 'mongodb://localhost/NiceFind',
+      options: {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        reconnectTries: Number.MAX_VALUE,
+        reconnectInterval: 500
+      }
+    },
+    jwt: {
+      secret: process.env.JWT_SECRET || 'development_secret',
+      expiry: '7d'
+    }
+  };
